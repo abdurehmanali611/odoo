4f66c60c889940acaed04d3aff01d6734f928551
abdurehmanali611 <abdurehmanali611@gmail.com>
2026-03-06 08:40:21 +0300

    Update Part 1

--- addons/office_asset/__manifest__.py
+++ addons/office_asset/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'category': 'Tools',
     'summary': 'Track office hardware and furniture',
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/asset_views.xml'
--- addons/office_asset/models/asset.py
+++ addons/office_asset/models/asset.py
@@ -1,10 +1,25 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date
 
 class OfficeAsset(models.Model):
     _name = 'office.asset'
     _description = 'Office Asset'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     name = fields.Char(string="Device Name", required=True)
     serial_number = fields.Char(string="Serial Number")
+    price = fields.Float(string="Purchase Price")
+    currency_id = fields.Many2one('res.currency', string="Currency", default=lambda self: self.env.company.currency_id)
+    age = fields.Integer(string="Asset Age (Days)", compute="_compute_asset_age")
+
+    @api.depends('purchase_date')
+    def _compute_asset_age(self):
+        for record in self:
+            if record.purchase_date:
+                delta = date.today() - record.purchase_date
+                record.age = delta.days
+            else:
+                record.age = 0
+    
     asset_type = fields.Selection([
         ('laptop', 'Laptop'),
         ('monitor', 'Monitor'),
@@ -20,4 +35,5 @@ class OfficeAsset(models.Model):
         ('damaged', 'Damaged')
     ], string="Status", default="new")
     notes = fields.Text(string="Internal Notes")
+    image = fields.Binary(string="Asset Image")
     
